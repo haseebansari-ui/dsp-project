8de15a84a79383bb8070599fd066a53410a52741
haseebansari-ui <haseeb.ansari@havmormedia.com>
2026-04-21 17:05:01 +0530

    pg add & css changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// ====================
+// 	Desktop Menue Bar
+// ====================
+
 var menueBar = document.querySelector(".menue-bar");
 var sideMenu = document.querySelector('.side-manue');
 var sideMenuClose = document.querySelector('.side-menu-close')
@@ -12,51 +16,10 @@ sideMenuClose.addEventListener("click", ()=>{
 	sideMenu.classList.remove("active");
 })
 
-// ===============
-// 	Login Form
-// ===============
-
-var passwordShow = document.querySelector(".hide-password");
-var passwordInput = document.querySelector(".input-field-psw");
-var passwordHideEye = document.querySelector(".hide-eye");
-var passwordShowEye = document.querySelector(".show-eye");
-
-passwordShow.addEventListener("click",()=>{
-  if(passwordInput.type === "password"){
-      passwordInput.type = "text";
-       passwordHideEye.style.display = "none";
-       passwordShowEye.style.display = "block";
-    }
-    else{
-      passwordInput.type = "password";
-      passwordShowEye.style.display = "none";
-      passwordHideEye.style.display = "block";      
-    }    
-})
-
-
-
 
 // ===============
-// 	Accordian
+// 	Sliders
 // ===============
-
-const accordian = document.querySelectorAll('.contentBx');
-
-for (let i = 0; i < accordian.length; i++) {
-	accordian[i].addEventListener('click', function () {
-		this.classList.toggle('active');
-	});
-}
-
-const header = document.querySelector("header");
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 0) {
-    header.classList.add("h-scroll");
-  } else {
-    header.classList.remove("h-scroll");
-  }
-});
 
 $(".services-slider").slick({
   infinite: true,
@@ -126,9 +89,61 @@ $(".posts-slider").slick({
   ]
 });
 
+
+// ===============
+// 	Scroll Header
+// ===============
+
+const header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    header.classList.add("h-scroll");
+  } else {
+    header.classList.remove("h-scroll");
+  }
+});
+
+
+
+// ===============
+// 	Accordian
+// ===============
+
+const accordian = document.querySelectorAll('.contentBx');
+
+for (let i = 0; i < accordian.length; i++) {
+	accordian[i].addEventListener('click', function () {
+		this.classList.toggle('active');
+	});
+}
+
+
 // script should be placed at the end of the all scripts
 // AOS
 AOS.init();
 $(window).on("load", function () {
   AOS.refresh();
 });
+
+// ===============
+// 	Login Form
+// ===============
+
+var passwordShow = document.querySelector(".hide-password");
+var passwordInput = document.querySelector(".input-field-psw");
+var passwordHideEye = document.querySelector(".hide-eye");
+var passwordShowEye = document.querySelector(".show-eye");
+
+passwordShow.addEventListener("click",()=>{
+  if(passwordInput.type === "password"){
+      passwordInput.type = "text";
+       passwordShowEye.style.display = "block";
+       passwordHideEye.style.display = "none";
+    }
+    else{
+      passwordInput.type = "password";
+      passwordHideEye.style.display = "block";
+      passwordShowEye.style.display = "none";
+            
+    }    
+})
